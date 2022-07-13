2cd7b1cf21699b6a978171614c8e13e18e73c884
mungaben <kamaumungai3742@gmail.com>
2022-07-13 15:50:10 +0300

    modified template to add data from database

--- mysite/Blog/views.py
+++ mysite/Blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 def post_list(request):
+    posts = Post.objects.all()
+    
+    
     context={
-        
+        "posts":posts
     }
     return render(request,"Blog/post_list.html",context)
 # Create your views here.
